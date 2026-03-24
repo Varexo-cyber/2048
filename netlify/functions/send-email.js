afceb014715ad7d114540d9c9ad8f31ddb5d71de
Varexo-cyber <info@varexo.nl>
2026-03-24 21:32:25 +0100

    Update send-email.js

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer')
 
 // Nodemailer transporter configuratie voor varexo.nl
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'mail.varexo.nl',
   port: process.env.SMTP_PORT || 587,
   secure: false,
